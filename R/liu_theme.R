246b913335d50525f1720bc1bb9724a255ff2d94
Hugo Morvan <98044151+hugo-morvan@users.noreply.github.com>
2023-09-26 10:38:03 +0200

    theme code start

---
+++ R/liu_theme.R
@@ -0,0 +1,8 @@
+liu_theme <- function() {
+  theme(
+    #starting code for the LiU theme, needs to be modified to fit the LiU theme
+    text = element_text(size = 12, family = "Arial"),
+    axis.title = element_text(face = "bold", size = 14),
+    panel.grid.major = element_line(color = "gray", size = 0.2)
+  )
+}
